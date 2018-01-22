fa46df0cfab70f7ea8aad503d60b78599dfbffb6
Telolahy <stephano.telolahy@gmail.com>
2018-01-23 00:30:02 +0100

    Set plash configuration

--- XConfigDemo/ViewController.swift
+++ XConfigDemo/ViewController.swift
@@ -8,23 +8,31 @@
 
 import UIKit
 
+class Constants {
+    
+    
+}
+
 class ViewController: UIViewController {
     
     @IBOutlet weak var infoLabel: UILabel!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         
-        guard let configs = Bundle.main.infoDictionary,
-            let useDebugControls = configs["USE_DEBUG_CONTROLS"] as? String,
-            let server = configs["SERVER_NAME"] as? String else {
+        guard let useDebugControls = info(forKey: "USE_DEBUG_CONTROLS"),
+            let serverUrl = info(forKey: "SERVER_URL") else {
                 return
         }
-        
         infoLabel.text = """
-                Use Debug controls: \(useDebugControls)
-                Server name: \(server)
-                """
+        Use Debug controls: \(useDebugControls)
+        Server: \(serverUrl)
+        """
+    }
+    
+    func info(forKey key: String) -> String? {
+        return (Bundle.main.infoDictionary?[key] as? String)?
+            .replacingOccurrences(of: "\\", with: "")
     }
     
 }
